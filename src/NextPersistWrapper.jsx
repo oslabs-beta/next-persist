1f4945d11768f719e206a6d5603c85342170679f
Gregory Levine-Rozenvayn <grisha617@gmail.com>
2021-04-12 14:10:25 -0400

    comments added to NextPersistWrapper and next-persist files

--- src/NextPersistWrapper.jsx
+++ src/NextPersistWrapper.jsx
@@ -2,16 +2,24 @@ import { Component } from 'react';
 import { connect } from 'react-redux';
 import { writeStorage } from './next-persist';
 
+// mapStateToProps allows for access to state
 const mapStateToProps = (state) => ({
   state,
 });
 
+// This componenet should live in _app between Provider and children components
 class NextPersistWrapper extends Component {
   render() {
+    // if combinedReducers value is true:
+    // loop through each reducer and write their respective returned objects to state,
     if (this.props.wrapperConfig.combinedReducers) {
+      // allowedKeys is an array that's defined by developer and
+      // will be the keys that are saved to localStorage
       const { allowedKeys } = this.props.wrapperConfig;
+      // allowedReducers is an array defined by developer that are the values saved on localStorage
+      // with their corresponding key in allowedKeys array
       const { allowedReducers } = this.props.wrapperConfig;
-
+      // writeStorage method must be called for each reducer in allowedReducers array
       allowedReducers.forEach((allowedReducer, index) => {
         const nextPersistConfig = {
           key: allowedKeys[index],
@@ -19,13 +27,14 @@ class NextPersistWrapper extends Component {
         writeStorage(nextPersistConfig, this.props.state[allowedReducer]);
       });
     } else {
+      // if combinedReducer value is false:
+      // writeStorage method only needs to be called once
       const nextPersistConfig = {
         key: this.props.wrapperConfig.key,
         allowList: this.props.wrapperConfig.allowList,
       };
       writeStorage(nextPersistConfig, this.props.state);
     }
-
     return this.props.children;
   }
 }
--- src/next-persist.js
+++ src/next-persist.js
@@ -1,13 +1,20 @@
 const nextPersist = {};
 
-// writes to local storage
+// this method writes to local storage
+// Args to pass in:
+// nextPersistConfig: type: object, defined in _app
+// state: type: object, defined in store
 nextPersist.writeStorage = (nextPersistConfig, state) => {
   const { key, allowList } = nextPersistConfig;
 
+  // if window is defined, there is access local storage
   if (typeof window !== 'undefined') {
+    // if allowList was not defined in persistConfig, set all items from state to localStorage
     if (!allowList) {
       localStorage.setItem(key, JSON.stringify(state));
     } else {
+      // for every element in allowList:
+      // add to new state object and set to local storage
       const allowedState = allowList.reduce((acc, cur) => {
         acc[cur] = state[cur];
         return acc;
@@ -15,14 +22,22 @@ nextPersist.writeStorage = (nextPersistConfig, state) => {
       localStorage.setItem(key, JSON.stringify(allowedState));
     }
   } else {
+    // if window is undefined, return error object
     return { err: 'LocalStorage not found.' };
   }
 };
 
-// retrieves from local storage
+// This method retrieves state from local storage
+// Arguements to pass in:
+// key: type: string, defined in reducer file
+// state: type: object, defined in reducer file
 nextPersist.getStorage = (key, state) => {
+  // if window is defined there is access local storage,
+  // otherwise return same state object that was passed in
   if (typeof window !== 'undefined') {
     const clientState = localStorage.getItem(key);
+    // if clientState object exists on localStorage, then return this clientState object,
+    // otherwise return the state object that was passed in to getStorage method
     if (clientState) {
       const parsedClientState = JSON.parse(clientState);
       return {
